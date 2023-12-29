0193837321a3503a2b621b1557221982a084d3d6
usernamenenad <radovic.nenad158@gmail.com>
2023-12-29 08:20:42 +0100

    HW3: Fixes, fixes...

--- homework3/src/blackjack/td.py
+++ homework3/src/blackjack/td.py
@@ -28,6 +28,19 @@ class QLearning(TD, Observer):
     def __init__(self, q: Q = None, gamma: float = 1.0, alpha: float = 0.1):
         super().__init__(q if q else Q(), gamma, alpha)
 
+    def update(self, *new_state):
+        s = new_state[0][0]
+        new_s = new_state[0][1]
+        a = new_state[0][2]
+        r = new_state[0][3]
+
+        if new_s:
+            v_plus = self.q.determine_v(new_s)
+        else:
+            v_plus = 0
+
+        self.q[s, a] = (1 - self.alpha) * self.q[s, a] + self.alpha * (r + self.gamma * v_plus)
+
     def run(self, game: Game, iterations: int) -> Q:
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         print("Starting Q-Learning...")
@@ -44,21 +57,6 @@ class QLearning(TD, Observer):
 
             for player in game.players:
                 for rnd in player.experiences:
-                    for j, part in enumerate(player.experiences[rnd].experience):
-                        s, a = part[0], part[1]
-                        if j + 1 < len(player.experiences[rnd].experience):
-                            r = 0.0
-                            v_plus = self.q.determine_v(player.experiences[rnd][j + 1][0])
-                        else:
-                            r = part[2]
-                            v_plus = 0.0
-
-                        self.q[s, a] = (1 - self.alpha) * self.q[s, a] + self.alpha * (r + self.gamma * v_plus)
-
-                    # This DOES NOT mean that we're going to forget experiences.
-                    # All experiences of this game are
-                    # transferred to `occurrences` dictionary, and we
-                    # clear experiences for the next game.
                     player.experiences[rnd].clear()
 
         print("Finished Q-Learning!")
