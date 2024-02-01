a1c5b0abef2bd1040fbf6c53cbfcb8cac21d3457
usernamenenad <radovic.nenad158@gmail.com>
2024-02-01 18:31:47 +0100

    HW3: Code refactor

--- homework3/tests/test_agents.py
+++ homework3/tests/test_agents.py
@@ -5,14 +5,14 @@ def test_agents():
     card_dealer = Card(CardNumber.SIX, CardSuit.CLUB)
     print(f"Dealer's card: {card_dealer}")
 
-    dealer = Dealer(state=DealerState())
+    dealer = Dealer(state=State())
     dealer.update_total(card=card_dealer)
     print(f"Agent: {dealer}, state: {dealer.state}\r\n")
 
     card_player = Card(CardNumber.ACE, CardSuit.DIAMOND)
     print(f"Player's card: {card_player}")
 
-    player = Player(state=PlayerState())
+    player = Player(state=State())
     player.update_total(card=card_player)
     print(f"Agent: {player}, state: {player.state}\r\n")
 
--- homework3/tests/test_game.py
+++ homework3/tests/test_game.py
@@ -6,7 +6,7 @@ def test_game():
     players = [Player() for _ in range(no_players)]
     game = Game(players)
 
-    game.play(policy=RandomPolicy(), gamma=0.9, q=Q())
+    game.play(gamma=0.9, q=Q())
     Info.draw_experience(game, rnd=2)
 
     Info.log_experiences(game.players)
